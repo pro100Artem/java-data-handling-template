5b456b6491e97e0ca17c07e544f229d3aaef8ef0
Artem <artem2019.r@mail.ru>
2021-06-04 14:49:57 +0400

    Good

--- src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
@@ -1,5 +1,9 @@
 package com.epam.izh.rd.online.service;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleTextService implements TextService {
 
     /**
@@ -13,7 +17,15 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public String removeString(String base, String remove) {
-        return null; //TODO
+        Pattern pattern = Pattern.compile(remove);
+        Matcher matcher = pattern.matcher(base);
+        String removeBase = "";
+        if (matcher.find()) {
+            removeBase = matcher.replaceAll("");
+        } else {
+            return base;
+        }
+        return removeBase; //TODO
     }
 
     /**
@@ -24,6 +36,11 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public boolean isQuestionString(String text) {
+        Pattern pattern = Pattern.compile(".$");
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            return matcher.group().equals("?");
+        }
         return false; //TODO
     }
 
@@ -35,7 +52,12 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public String concatenate(String... elements) {
-        return null; //TODO
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < elements.length; i++) {
+            builder.append(elements[i]);
+        }
+        String returnedString = String.valueOf(builder);
+        return returnedString; //TODO
     }
 
     /**
@@ -47,7 +69,22 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public String toJumpCase(String text) {
-        return null; //TODO
+        String textToLowerCase = text.toLowerCase();
+        String textToUpperCase = text.toUpperCase();
+        char[] charsLower = textToLowerCase.toCharArray();
+        char[] charsUpper = textToUpperCase.toCharArray();
+        char[] charsArray = new char[charsLower.length];
+        for (int i = 0; i < charsLower.length; i++) {
+            if (i % 2 == 0) {
+                charsArray[i] = charsLower[i];
+            } else {
+                charsArray[i] = charsUpper[i];
+            }
+        }
+        String fes = String.valueOf(charsArray);
+
+
+        return fes; //TODO
     }
 
     /**
@@ -59,6 +96,14 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public boolean isPalindrome(String string) {
-       return false; //TODO
+        String palindrome = string.replace(" ", "").toLowerCase();
+        StringBuilder palindromeBuilder = new StringBuilder(palindrome);
+        StringBuilder palindromeBuilderReverse = palindromeBuilder.reverse();
+        String palindromeReverse = String.valueOf(palindromeBuilderReverse);
+        if (!string.equals("")) {
+            return palindrome.equals(palindromeReverse);
+        } else {
+            return false;
+        }
     }
 }
